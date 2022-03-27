b6bb4fb8ab7a9bba887211338393e73f765bc565
meohyun24 <84959523+meohyun@users.noreply.github.com>
2022-03-27 20:16:53 +0900

    Add files via upload

---
+++ LgTV.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class LgTV implements TV{
+	public void powerOn() {
+		System.out.println("LgTV-- 전원켠다.");
+	}
+	public void powerOff() {
+		System.out.println("LgTV-- 전원끈다.");
+	}
+	public void volumeUp() {
+		System.out.println("LgTV-- 소리올린다.");
+	}
+	public void volumeDown() {
+		System.out.println("LgTV-- 소리내린다.");
+	}
+}
